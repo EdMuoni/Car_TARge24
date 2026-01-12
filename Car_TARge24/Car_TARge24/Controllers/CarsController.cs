e59c6503074b8d9a98bff5e8708f5532692bf8bd
Edgar <edgar.muoni@gmail.com>
2026-01-12 13:17:36 +0200

    Controller CreateUpdate

--- Car_TARge24/Car_TARge24/Controllers/CarsController.cs
+++ Car_TARge24/Car_TARge24/Controllers/CarsController.cs
@@ -9,11 +9,59 @@ using Car_TARge24.Core.ServiceInterface;
 namespace Car_TARge24.Controllers
 
 {
-    public class CarsController: Controller
+    public class CarsController : Controller
     {
 
-        //private readonly Car_TARge24Context _context;
-        //private readonly ICarServices _carServices;
+        private readonly Car_TARge24Context _context;
+        private readonly ICarServices _carServices;
 
-    }
+        public CarsController
+            (
+             Car_TARge24Context context,
+             ICarServices carServices
+            )
+        {
+            _context = context;
+            _carServices = carServices;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Cars
+                .Select(x => new CarViewModel
+                {
+                    Id = x.Id,
+                    Brand = x.Brand,
+                    Model = x.Model,
+                    EnginePower = x.EnginePower,
+                    Year = x.Year,
+                });
+
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            CarCreateUpdateViewModel result = new();
+
+            return View("CreateUpdate", result);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(CarCreateUpdateViewModel vm)
+        {
+            CarDto dto = new()
+            {
+                Brand = vm.Brand,
+                Model = vm.Model,
+                Color = vm.Color,
+                EnginePower = vm.EnginePower,
+                Year = vm.Year,
+                FuelConsumption = vm.FuelConsumption,
+            };
+            await _carServices.Create(dto);
+            return RedirectToAction("Index");
+        }
 }
