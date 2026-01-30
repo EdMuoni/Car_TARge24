2409dd23905cb24f9ff617bfa0d97ca5259b60c6
Edgar <edgar.muoni@gmail.com>
2026-01-30 13:55:56 +0200

    Creating test and testing

--- Car_TARge24/TARge24_Car_Test/Car_Test.cs
+++ Car_TARge24/TARge24_Car_Test/Car_Test.cs
@@ -32,7 +32,7 @@ namespace TARge24_Car_Test
             Assert.NotEqual(wrongGuid, guid);
         }
 
-        //Kontrollime, et õige ID-ga lasteaed saab kätte
+        //Kontrollime, et õige ID-ga car saab kätte
         [Fact]
         public async Task Should_GetByIdcar_WhenReturnsEqual()
         {
@@ -40,12 +40,50 @@ namespace TARge24_Car_Test
             Guid databaseGuid = Guid.Parse("82491449-257a-48ff-81b9-3a95f925ce05");
             Guid guid = Guid.Parse("82491449-257a-48ff-81b9-3a95f925ce05");
             //act
-            await Svc<IcarServices>().DetailAsync(guid);
+            await Svc<ICarServices>().DetailAsync(guid);
             //assert
             Assert.Equal(databaseGuid, guid);
         }
 
+        //Kontrollime, et negatiivse EnginePower ei saa lisada
+        [Fact]
+        public async Task ShouldNot_AddCarWithNegativeEnginePower_WhenReturnResult()
+        {
+            // Arrange
+            CarDto dto = new()
+            {
+                Brand = "Volvo",
+                Model = "S",
+                Color = "Red",
+                EnginePower = -2000,
+                Year = 2020,
+                FuelConsumption = 15,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+            // Act
+            var result = await Svc<ICarServices>().Create(dto);
+            // Assert
+            Assert.NotNull(result);
+        }
 
+
+        //Kontrollime, et car andmeid saab uuendada
+        [Fact]
+        public async Task Should_UpdateCar_WhenUpdateKindergartenData()
+        {
+            //arrange
+            CarDto dto = MockCarData();
+            //act
+            var createKindergarten = await Svc<ICarServices>().Create(dto);
+            var updateKindergarten = MockUpdateCarData();
+            //assert
+            Assert.NotEqual(createKindergarten.EnginePower, updateKindergarten.EnginePower);
+            Assert.NotEqual(createKindergarten.Year, updateKindergarten.Year);
+            Assert.NotEqual(createKindergarten.FuelConsumption, updateKindergarten.FuelConsumption);
+            Assert.NotEqual(createKindergarten.Color, updateKindergarten.Color);
+
+        }
 
         private CarDto MockCarData()
         {
@@ -68,10 +106,10 @@ namespace TARge24_Car_Test
             {
                 Brand = "Subaru",
                 Model = "S",
-                Color = "Red",
+                Color = "Pink",
                 EnginePower = 8000,
                 Year = 2025,
-                FuelConsumption = 15,
+                FuelConsumption = 5,
                 CreatedAt = DateTime.UtcNow,
                 UpdatedAt = DateTime.UtcNow
             };
